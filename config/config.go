8845f6a5bf30e6b1eb5afcf55dcbe254f9ed9472
Gerardo A. Bolanos <gbolanos.dev@gmail.com>
2026-04-03 16:23:17 -0700

    Feature: load api key from ~/.worklog/config.yaml

--- config/config.go
+++ config/config.go
@@ -1,1 +1,42 @@
 package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Anthropic Anthropic `yaml:"anthropic"`
+}
+
+type Anthropic struct {
+	APIKey string `yaml:"api_key"`
+}
+
+func Load() (*Config, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	dir := filepath.Join(home, ".worklog")
+	err = os.MkdirAll(dir, 0755)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	data, err := os.ReadFile(filepath.Join(dir, "config.yaml"))
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
